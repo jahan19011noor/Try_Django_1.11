a583959bd751bb4dd2e37c745a986687eb15429c
jahan19011noor <jahannoor19011@gmail.com>
2018-03-29 17:38:59 +0600

    9. Template Inheritance - Completed

--- project/trydjango1_11/restaurants/views.py
+++ project/trydjango1_11/restaurants/views.py
@@ -7,8 +7,7 @@ from django.shortcuts import render
 
 # Create your views here.
 # function based view
-
-def home(request):
+# def home(request):
 
 # up to chapter 5: For your Reference
     # return HttpResponse("Home Page")
@@ -31,11 +30,31 @@ def home(request):
     # return HttpResponse(html_)
 # up to chapter 5: Rendering HTML
 
-# up to chapter 6: Render a Django Template
+# up to chapter 6. Render a Django Template and 7. Render a Django Template and 8. Context in Django Templates
 
+    # num = random.randint(0, 10000000)
+    # list_of_values = ["one", "two", "three"]
+    #
+    # return render(request, "base.html", {"title":"Homepage", "show_the_random_number": True, "num": num, "values": list_of_values})
+
+# up to chapter 6. Render a Django Template and 7. Render a Django Template and 8. Context in Django Templates
+
+# up to chapter 8. Template Inheritance
+def home(request):
     num = random.randint(0, 10000000)
     list_of_values = ["one", "two", "three"]
 
-    return render(request, "base.html", {"title":"Homepage", "show_the_random_number": True, "num": num, "values": list_of_values})
+    return render(request, "home.html", {"title":"Homepage", "show_the_random_number": True, "num": num, "values": list_of_values})
 
-# up to chapter 6: Render a Django Template
+def contact(request):
+    num = random.randint(0, 10000000)
+    list_of_values = ["one", "two", "three"]
+
+    return render(request, "contact.html", {"title":"Home 1", "show_the_random_number": True, "num": num, "values": list_of_values})
+
+def about(request):
+    num = random.randint(0, 10000000)
+    list_of_values = ["one", "two", "three"]
+
+    return render(request, "about.html", {"title":"Home 2", "show_the_random_number": True, "num": num, "values": list_of_values})
+# up to chapter 8. Template Inheritance
