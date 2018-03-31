0ce40e89a2d3aab8d513f40a2790e770cc4f1acd
System Administrator <root@Noorjahans-MacBook-Pro.local>
2018-04-01 00:02:55 +0600

    15. More on Model Fields - Completed

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,5 +5,9 @@ from django.db import models
 
 # Create your models here.
 class Restaurant(models.Model):
-    name        = models.CharField(max_length=100)
-    location    = models.CharField(max_length=255, null=True, blank=True)
+    name            = models.CharField(max_length=100)
+    location        = models.CharField(max_length=255, null=True, blank=True)
+    catagory        = models.CharField(max_length=255, null=True, blank=True)
+    timestamp       = models.DateTimeField(auto_now_add=True)   # Saves automatically and does not allow to make changes
+    updated         = models.DateTimeField(auto_now=True)       # Saves automatically and does not allow to make changes
+    # my_date_field   = models.DateTime(auto_now=False, auto_now_add=False)
