e2f9079f7503e6e21983dc3e3c3e324a835f7a11
jahan19011noor <jahannoor19011@gmail.com>
2018-04-16 16:56:22 +0600

    21. Signal for Unique Slug - Completed

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+from .utils import unique_slug_generator
 
 # Create your models here.
 class Restaurant(models.Model):
@@ -14,8 +14,26 @@ class Restaurant(models.Model):
     # my_date_field   = models.DateTime(auto_now=False, auto_now_add=False)
 
     def __str__(self):
-        return self.catagory
+        return self.name
 
     @property
     def title(self):
         return self.name
+
+
+def r_pre_save_receiver(sender, instance, *args, **kwargs):
+    print ('saving..')
+    print (instance.timestamp)
+    if not instance.slug:               #always do pre_save
+        instance.slug = unique_slug_generator(instance)
+
+# def r_post_save_receiver(sender, instance, *args, **kwargs):
+#     print ('saved..')
+#     print (instance.timestamp)
+#     # instance.save()                     #not a recommanded way to save because it will run into an infinite loop as save() will recall post_save()
+#     if not instance.slug:                 #this will never be called because the presave will already set the instance.slug, so infinite loop will be avoided
+#         instance.slug = unique_slug_generator(instance)
+#         instance.save()
+
+pre_save.connect(r_pre_save_receiver, sender=Restaurant)
+# post_save.connect(r_post_save_receiver, sender=Restaurant)
