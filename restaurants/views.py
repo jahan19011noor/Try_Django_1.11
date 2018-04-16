51dbf0fdb30888588c6a1c04dcc5e03ab1831429
jahan19011noor <jahannoor19011@gmail.com>
2018-04-16 17:42:00 +0600

    rearrangement after chap: 22. Slugs as URL Params

--- restaurants/views.py
+++ restaurants/views.py
@@ -16,36 +16,14 @@ def restaurang_list_view(request):
     return render(request, template_name=template_name, context=context)
 
 class RestaurantListView(ListView):
-    # built in attributes of ListView:
-    # queryset      // not querySet or QuerySet
-    # template_name // not template_Name or templateName
-    # object_list   // not object_List or objectList
-    #           // object_list gets assigned automatically with queryset
-    #           // object_list gets assigned dynaically assigned to the context dict
-    #           // thus, object_list gets sent to the html and has to be received with the same name there
-
-    # overriding queryset and template_name
     queryset = Restaurant.objects.all()
-    # template_name = 'restaurants/restaurant_view.html'
-    # if not overridden then it will show errro 'template_name' missing or 'QuerySet' missing
 
 class RestaurantDetailView(DetailView):
 
     queryset = Restaurant.objects.all()
-    # lets view the context that gets loaded by the Generic DetailView by default - overriding get_context_data() o super
     def get_context_data(self, *args, **kwargs):
+        # queries field_name = value - where field_name = url_param_name and value = value
         print(self.kwargs)
         context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)   # getting the super's get_context_data
         print(context)  # print the supers context
         return context  # have to return to complete overriding
-
-# overriding get_object for DetailView to query with "restaurant_id"
-#     def get_object(self, *args, **kwargs):
-#         print(self.kwargs)
-#         rest_id = self.kwargs.get('restaurant_id')
-#         obj = get_object_or_404(Restaurant, id=rest_id)
-#         return obj
-# overriding get_object for DetailView to query with "restaurant_id"
-
-    #******* do not have to provide template_name because the default template name for DetailView is modelName_detail.html
-    # template_name = 'restaurants/restaurant_detail.html'
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -1,18 +1,3 @@
-"""trydjango1_11 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 
@@ -25,23 +10,12 @@ from restaurants.views import (
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-# up to chapter 19: Restaurant Profile Detail
-
     url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^about/$', TemplateView.as_view(template_name='about.html')),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
-    # url(r'^restaurants/$', restaurang_list_view),
-    # url(r'^restaurants/(?P<slug>\w+)/$', SearchRestaurantListView.as_view()),
     url(r'^restaurants/$', RestaurantListView.as_view()),
 
-# pk or slug is the default for regular expression to query modal detail
-    # to change to something else override the get_object for the DetailView
-    # url(r'^restaurants/(?P<pk>\w+)/$', RestaurantDetailView.as_view()),
-    # url(r'^restaurants/(?P<restaurant_id>\w+)/$', RestaurantDetailView.as_view()),
+    # after chapter 22. Slugs as URL Params
     url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
-# pk or slug is the default for regular expression to query modal detail
-    # to change to something else override the get_object for the DetailView
-
-# up to chapter 19: Restaurant Profile Detail
+    # after chapter 22. Slugs as URL Params
 ]
-# .as_view() creates an instance fo the class ContactView so that the class can be used
