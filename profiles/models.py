1e20c4d0c6d96b13bd2a7554a5fe6ec423db9f02
Noorjahan Mukammel <jahan@Noorjahans-MacBook-Pro.local>
2018-05-11 02:14:08 +0600

    profile to user - following relations modified my Noor Jahan

--- profiles/models.py
+++ profiles/models.py
@@ -6,8 +6,7 @@ User = settings.AUTH_USER_MODEL
 
 class Profile(models.Model):
     user        = models.OneToOneField(User) #user.profile accesses profile and reverse
-    followers   = models.ManyToManyField(User, related_name='i_am_following', blank=True) #user.i_am_following.all()
-    # following   = models.ManyToManyField(User, related_name='following', blank=True) #user.following.all()
+    following   = models.ManyToManyField(User, related_name='followers', blank=True)
     activated   = models.BooleanField(default=False)
     timestamp   = models.DateTimeField(auto_now_add=True)
     updated     = models.DateTimeField(auto_now=True)
@@ -18,9 +17,8 @@ class Profile(models.Model):
 def post_save_user_receiver(sender, instance, created, *args, **kwargs):
     if created:
         profile, is_created = Profile.objects.get_or_create(user=instance)
-        default_user_profile = Profile.objects.get_or_create(user__id=1)[0]
-        default_user_profile.followers.add(instance)
-        profile.followers.add(default_user_profile.user)
-        profile.followers.add(2)
+        the_super_user_profile = Profile.objects.get_or_create(user__id=1)[0]
+        profile.following.add(the_super_user_profile.user)
+        the_super_user_profile.following.add(instance)
 
 post_save.connect(post_save_user_receiver, sender=User)
